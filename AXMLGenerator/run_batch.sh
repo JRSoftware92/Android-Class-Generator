a05d1459405fe78928d3aeaad8987d1579feabfc
JRSoftware92 <memorycalcandroid@gmail.com>
2015-09-25 17:13:16 -0400

    Removing more useless code

--- AXMLGenerator/run_batch.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#This program will generate a batch of android layout classes 
-#based on their xml layout files and java class templates.
-#Requires a batch file where each line has the syntax input:output:template
-BATCH_FILE=/input/test_activity_batch.txt
-ruby src/main/run_generation.rb -b $BATCH_FILE
--- AXMLGenerator/run_generator.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#This program will generate an android layout class
-#based on its xml layout file and java class template.
-INPUT_FILE=/input/activity_test.xml
-OUTPUT_FILE=/output/activity_test.java
-TEMPLATE_FILE=/templates/ActivityTemplate.java
-ruby src/main/run_generation.rb -f $INPUT_FILE $OUTPUT_FILE $TEMPLATE_FILE
